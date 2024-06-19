41416861b978a6a067bc3906b83777bcc4da1a18
girish <girishiva1989@gmail.com>
2024-06-20 09:34:53 +1000

    Added echo statements in catalogue for color coding Updated ip address in roboshop.conf

--- catalogue.sh
+++ catalogue.sh
@@ -1,29 +1,41 @@
+echo -e "\e[31m<<<<<<<<<List the modules and enable 18 version>>>>>>>>>\e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
+echo -e "\e[31m<<<<<<<<<IInstall NodeJS>>>>>>>>>\e[0m"
 dnf install nodejs -y
 
+echo -e "\e[31m<<<<<<<<<Add application User to configure application>>>>>>>>\e[0m"
 useradd roboshop
 
+echo -e "\e[31m<<<<<<<<<Setup an app directory>>>>>>>>>\e[0m"
+rm -rf /app
 mkdir /app
 
+echo -e "\e[31m<<<<<<<<<Download the application code to created app directory>>>>>>>>>\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
 
+echo -e "\e[31m<<<<<<<<<Download the dependencies>>>>>>>>>\e[0m"
 npm install
 
-cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[31m<<<<<<<<<Setup SystemD Catalogue Service>>>>>>>>>\e[0m"
+cp /home/centos/roboshop-shell-new/catalogue.service /etc/systemd/system/catalogue.service
 
+echo -e "\e[31m<<<<<<<<<Load and Start the service>>>>>>>>>\e[0m"
 systemctl daemon-reload
-
 systemctl enable catalogue
 systemctl start catalogue
 
-cp mongod.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[31m<<<<<<<<<setup MongoDB repo>>>>>>>>>\e[0m"
+cp /home/centos/roboshop-shell-new/mongod.repo /etc/yum.repos.d/mongo.repo
 
+echo -e "\e[31m<<<<<<<<<Install mongodb-client>>>>>>>>>\e[0m"
 dnf install mongodb-org-shell -y
 
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+echo -e "\e[31m<<<<<<<<<load the schema>>>>>>>>>\e[0m"
+mongo --host mongod-dev.gdevops89.online </app/schema/catalogue.js
 
+echo -e "\e[31m<<<<<<<<<Restart Service>>>>>>>>>\e[0m"
 systemctl restart catalogue
