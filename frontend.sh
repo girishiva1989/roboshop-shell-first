6a800a9869732671cfc47cc93a2436e8746e5948
girish <girishiva1989@gmail.com>
2024-06-20 09:04:27 +1000

    Updated the script with echo commands by adding color codes

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,22 @@
+echo -e "\e[31m<<<<<<<<<Install nginx>>>>>>>>>\e[0m"
 dnf install nginx -y
 
+echo -e "\e[31m<<<<<<<<<Start & Enable Nginx service>>>>>>>>>\e[0m"
 systemctl enable nginx
 systemctl start nginx
 
+echo -e "\e[31m<<<<<<<<<Remove the default content that web server is serving>>>>>>>>>\e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[31m<<<<<<<<<Download the frontend content>>>>>>>>>\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[31m<<<<<<<<<Extract the frontend content>>>>>>>>>\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "\e[31m<<<<<<<<<Create Nginx Reverse Proxy Configuration>>>>>>>>\e[0m"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 
+echo -e "\e[31m<<<<<<<<<Restart Nginx Service>>>>>>>>>\e[0m"
 systemctl restart nginx
