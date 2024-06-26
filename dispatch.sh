0baf60c63afcbfbfab53badfc588e005b848a0dd
girish <girishiva1989@gmail.com>
2024-06-26 11:15:03 +1000

    Added color statements Added IP address

--- dispatch.sh
+++ dispatch.sh
@@ -1,20 +1,26 @@
+echo -e "\e[31m<<<<<<<<<Install GoLang>>>>>>>>>\e[0m"
 dnf install golang -y
 
+echo -e "\e[31m<<<<<<<<<Add application User>>>>>>>>>\e[0m"
 useradd roboshop
 
+echo -e "\e[31m<<<<<<<<<Setup app directory>>>>>>>>>\e[0m"
 mkdir /app
 
+echo -e "\e[31m<<<<<<<<<Download the application code>>>>>>>>>\e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
 unzip /tmp/dispatch.zip
 
+echo -e "\e[31m<<<<<<<<<Download the dependencies>>>>>>>>>\e[0m"
 go mod init dispatch
 go get
 go build
 
-cp dispatch.service /etc/systemd/system/dispatch.service
+echo -e "\e[31m<<<<<<<<<Setup SystemD Dispatch Service>>>>>>>>>\e[0m"
+cp /home/centos/roboshop-shell-first/dispatch.service /etc/systemd/system/dispatch.service
 
+echo -e "\e[31m<<<<<<<<<Load and Start the service>>>>>>>>>\e[0m"
 systemctl daemon-reload
-
 systemctl enable dispatch
 systemctl start dispatch
--- payment.sh
+++ payment.sh
@@ -1,11 +1,10 @@
-echo -e "\e[31m<<<<<<<<<nstall Python 3.6>>>>>>>>>\e[0m"
+echo -e "\e[31m<<<<<<<<<Install Python 3.6>>>>>>>>>\e[0m"
 dnf install python36 gcc python3-devel -y
 
 echo -e "\e[31m<<<<<<<<<Add application User>>>>>>>>>\e[0m"
 useradd roboshop
 
 echo -e "\e[31m<<<<<<<<<Setup app directory>>>>>>>>>\e[0m"
-rm -rf /app
 mkdir /app
 
 echo -e "\e[31m<<<<<<<<<Download the application code>>>>>>>>>\e[0m"
@@ -16,10 +15,10 @@ unzip /tmp/payment.zip
 echo -e "\e[31m<<<<<<<<<Download the dependencies>>>>>>>>>\e[0m"
 pip3.6 install -r requirements.txt
 
-echo -e "\e[31m<<<<<<<<<Configure YUM Repos>>>>>>>>>\e[0m"
-cp /home/centos/roboshop-shell-first/payment.service /etc/systemd/system/payment.service
+echo -e "\e[31m<<<<<<<<<Setup SystemD Payment Service>>>>>>>>>\e[0m"
+cp payment.service /etc/systemd/system/payment.service
 
-echo -e "\e[31m<<<<<<<<<Load and Start the service>>>>>>>>\e[0m"
+echo -e "\e[31m<<<<<<<<<Load and Start the service>>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment
