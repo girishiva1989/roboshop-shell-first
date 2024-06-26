5c420dcfd4b8d61c37492aa34b48bfb837b62df9
girish <girishiva1989@gmail.com>
2024-06-26 10:18:50 +1000

    Added color statements Add the Required IP Address

--- shipping.sh
+++ shipping.sh
@@ -1,25 +1,35 @@
+echo -e "\e[31m<<<<<<<<<Install Maven>>>>>>>>>\e[0m"
 dnf install maven -y
 
+echo -e "\e[31m<<<<<<<<<Add application User>>>>>>>>>\e[0m"
 useradd roboshop
 
+echo -e "\e[31m<<<<<<<<<Setup app directory>>>>>>>>>\e[0m"
+rm -rf /app
 mkdir /app
 
+echo -e "\e[31m<<<<<<<<<Download the application code>>>>>>>>>\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
 unzip /tmp/shipping.zip
 
+echo -e "\e[31m<<<<<<<<<Download the dependencies>>>>>>>>>\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
-cp shipping.service /etc/systemd/system/shipping.service
+echo -e "\e[31m<<<<<<<<<Setup SystemD Shipping Service>>>>>>>>>\e[0m"
+cp /home/centos/roboshop-shell-first/shipping.service /etc/systemd/system/shipping.service
 
+echo -e "\e[31m<<<<<<<<<Load and Start the service>>>>>>>>>\e[0m"
 systemctl daemon-reload
-
 systemctl enable shipping
 systemctl start shipping
 
+echo -e "\e[31m<<<<<<<<<Install mysql client>>>>>>>>>\e[0m"
 dnf install mysql -y
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+echo -e "\e[31m<<<<<<<<<Load schema>>>>>>>>>\e[0m"
+mysql -h mysql-dev.gdevops89.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 
+echo -e "\e[31m<<<<<<<<<Restart Service>>>>>>>>>\e[0m"
 systemctl restart shipping
