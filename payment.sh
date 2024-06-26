01b55c32668cacb57c4e4b3581dea6d993a3be50
girish <girishiva1989@gmail.com>
2024-06-26 10:53:30 +1000

    Added color statements Added IP address

--- payment.sh
+++ payment.sh
@@ -1,18 +1,25 @@
+echo -e "\e[31m<<<<<<<<<nstall Python 3.6>>>>>>>>>\e[0m"
 dnf install python36 gcc python3-devel -y
 
+echo -e "\e[31m<<<<<<<<<Add application User>>>>>>>>>\e[0m"
 useradd roboshop
 
+echo -e "\e[31m<<<<<<<<<Setup app directory>>>>>>>>>\e[0m"
+rm -rf /app
 mkdir /app
 
+echo -e "\e[31m<<<<<<<<<Download the application code>>>>>>>>>\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
 unzip /tmp/payment.zip
 
+echo -e "\e[31m<<<<<<<<<Download the dependencies>>>>>>>>>\e[0m"
 pip3.6 install -r requirements.txt
 
-cp payment.service /etc/systemd/system/payment.service
+echo -e "\e[31m<<<<<<<<<Configure YUM Repos>>>>>>>>>\e[0m"
+cp /home/centos/roboshop-shell-first/payment.service /etc/systemd/system/payment.service
 
+echo -e "\e[31m<<<<<<<<<Load and Start the service>>>>>>>>\e[0m"
 systemctl daemon-reload
-
 systemctl enable payment
 systemctl start payment
