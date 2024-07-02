0812d5e915681b031defd5e07852e4a4a9f3df17
girish <girishiva1989@gmail.com>
2024-07-02 12:41:45 +1000

    Stored passwords in variables

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@ script=$(realpath $0)
 script_path=$(dirname $script)
 source ${script_path}/common.sh
 #RoboShop@1
-#mysql_user_password=$*
+mysql_user_password=$*
 
 dnf module disable mysql -y
 
@@ -17,8 +17,8 @@ systemctl enable mysqld
 systemctl start mysqld
 
 echo -e "\e[31m<<<<<<<<<Change the default root password>>>>>>>>>\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass ${mysql_user_password}
 
 echo -e "\e[31m<<<<<<<<<Check the new password is working>>>>>>>>>\e[0m"
-mysql -uroot -pRoboShop@1
+mysql -uroot -p${mysql_user_password}
 quit
